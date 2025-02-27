30c944ba7fd24c495db8654092a1741121b7a637
ShubhiNull <shoe.n.bee@gmail.com>
2025-02-27 10:14:02 +0100

    USER GUESSER WORKINGGG - Shubhi

--- Shubhi/user_guesser.py
+++ Shubhi/user_guesser.py
@@ -11,7 +11,6 @@ guesses = set()
 lowest_number = 0
 highest_number = 100
 while feedback != "correct":
-    print (your_guess)
     guesses.add(your_guess)
     if feedback == "lower":
         if highest_number > your_guess:
@@ -25,8 +24,11 @@ while feedback != "correct":
         print ("invalid input")
     #your_guess = random.randint(lowest_number, highest_number)
     your_guess = random.choice(list(set(range(lowest_number, highest_number)) - guesses))
+    print (your_guess)
     feedback = input("feedback? ")
     print (f" I've guessed: {guesses}")
+    if highest_number<lowest_number:
+        print ("LIAR")
     #your_guess = random.randint(0,10)   
 else: 
     print ("yay :)")
